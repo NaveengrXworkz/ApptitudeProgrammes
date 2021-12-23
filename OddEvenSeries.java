d81d77e10c6a8744aae1231fa15b5d478fe90532
NaveengrXworkz <naveengr.xworkz@gmail.com>
2021-12-23 21:47:12 +0530

    programmes to print odd or even and odd even series

---
+++ OddEvenSeries.java
@@ -0,0 +1,14 @@
+class OddEvenSeries{
+     public static void main(String []args){
+          int series = 1;
+   for(series=1; series<=20;series=series+1){
+        if (series %2==0){
+        System.out.println(series + " is even ");
+        }
+           else{
+              System.out.println(series + " is odd ");
+           }
+   }
+            
+     }
+}
